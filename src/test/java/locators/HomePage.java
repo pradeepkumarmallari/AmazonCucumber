0472f191a3fff4f1221e645d984375385820a793
pradeepkumarmallari <pradeepkumarmallari@gmail.com>
2019-07-09 23:52:19 +0530

    Comitted on 7/9/2019 at 11:52 PM

--- src/test/java/locators/HomePage.java
+++ src/test/java/locators/HomePage.java
@@ -1,9 +1,46 @@
 package locators;
 
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import reusableMethods.ReusableMethods;
+
+
 public class HomePage {
-
-	public HomePage() {
-		// TODO Auto-generated constructor stub
+	WebDriver driver;
+	ReusableMethods reusableMethods;
+	
+	@FindBy(xpath="//*[@id='nav-tools']/a[1]/span[2]")
+	WebElement hwrsigninTab;
+	
+	@FindBy(xpath="//*[@id='nav-flyout-ya-signin']/a[1]/span[1]")
+	WebElement btnSignIn;
+	
+	
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(this.driver, this);
+		reusableMethods=new ReusableMethods(this.driver);
+		
 	}
-
+	
+	public void howerToSignInTab() {
+		reusableMethods.explicitWait(hwrsigninTab, 20);
+		Actions action=new Actions(driver);
+		action.moveToElement(hwrsigninTab).build().perform();			
+	}
+	
+	public void clickOnSignOnButton() {
+		reusableMethods.explicitWait(btnSignIn, 20);
+		btnSignIn.click();
+	}
+	
+	
+	
+	
 }
--- src/test/java/reusableMethods/ReusableMethods.java
+++ src/test/java/reusableMethods/ReusableMethods.java
@@ -3,9 +3,13 @@ package reusableMethods;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ReusableMethods {
 	private WebDriver driver;
+	private WebDriverWait webDriverWait;
 	
 	public ReusableMethods(WebDriver driver) {
 		this.driver=driver;
@@ -15,5 +19,11 @@ public class ReusableMethods {
 		driver.get(url);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+	
+	public void explicitWait(WebElement element, int time) {
+		webDriverWait=new WebDriverWait(driver, time);
+		webDriverWait.until(ExpectedConditions.elementToBeClickable(element));
+		
 	}
 }
